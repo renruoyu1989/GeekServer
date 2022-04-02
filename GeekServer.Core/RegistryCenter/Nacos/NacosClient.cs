4275ac4cc0932149f027bc64679a27b3726dd6e3
leeveel <406351314@qq.com>
2022-04-02 14:38:24 +0800

    README.md

---
+++ GeekServer.Core/RegistryCenter/Nacos/NacosClient.cs
@@ -0,0 +1,19 @@
+﻿using Nacos.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek.Server
+{
+    public class NacosClient : SingletonTemplate<NacosClient>
+    {
+
+        private readonly INacosNamingService _nacosNamingService;
+
+
+
+
+    }
+}
---
+++ GeekServer.Gateway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeekServer.Gateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
